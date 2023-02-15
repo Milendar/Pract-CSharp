4ab9c9640fc9b0d702a1eab4b3073a23eaac0e5a
Millai <montallaar@gmail.com>
2023-02-15 15:57:40 +0300

    Practice

---
+++ Z34/Program.cs
@@ -0,0 +1,37 @@
+﻿int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+int HonestNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0) count++;
+    }
+    
+    return count;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]},");
+        else Console.Write(array[i]);
+    }
+    Console.WriteLine("]");
+}
+
+int[] arr = CreateArrayRndInt(10, 100, 999);
+int honestNumbers = HonestNumbers(arr);
+PrintArray(arr);
+Console.WriteLine($" -> {honestNumbers}");
